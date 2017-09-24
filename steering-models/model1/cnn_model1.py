f3cfd111bf4d66d48902871c50a450fdff0b3111
tenzin <ten13zin@gmail.com>
2017-09-24 17:24:43 +0530

    Add Helper function for max pooling

--- steering-models/model1/cnn_model1.py
+++ steering-models/model1/cnn_model1.py
@@ -17,3 +17,9 @@ def bias_variable(shape):
 
 def conv2d(x, W, stride):
     return tf.nn.con2d(x, W, strides=[1, stride, stride, 1], padding='SAME')
+
+
+def max_pooling_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1],
+                          padding='SAME')
