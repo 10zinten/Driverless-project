8c4dd4c47ae89780a86fb0c503b3f2f4c529916a
tenzin <ten13zin@gmail.com>
2017-09-24 17:16:03 +0530

    Add helper function to create weights and bias

---
+++ steering-models/model1/cnn_model1.py
@@ -0,0 +1,19 @@
+"""
+This CNN model1 is based on ../cnn_architecture.txt.
+"""
+
+import tensorflow as tf
+
+
+def weight_variable(shape):
+    initial = tf.truncate_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
+def conv2d(x, W, stride):
+    return tf.nn.con2d(x, W, strides=[1, stride, stride, 1], padding='SAME')
