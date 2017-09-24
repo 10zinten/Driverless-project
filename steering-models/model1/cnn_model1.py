2c8b9910514019fc7cdce2f5b3d69e04127336fd
tenzin <ten13zin@gmail.com>
2017-09-24 19:15:52 +0530

    Create CNN model 1

--- steering-models/model1/cnn_model1.py
+++ steering-models/model1/cnn_model1.py
@@ -44,6 +44,23 @@ def new_conv_layer(input,
     return layer
 
 
+def flatten_layer(layer):
+    # Get the shape of input layer
+    # - Shape of input layer: [num_images, img_height, img_width, num_channel]
+    layer_shape = layer.get_shape()
+
+    # Num. of features: img_height * img_width * num_channels
+    # Use TF function to calculate this
+    num_features = layer_shape[1:4].num_elements()
+
+    # Reshape the layer to [num_images, num_features]
+    layer_flat = tf.reshape(layer, [-1, num_features])
+
+    # flatten layer shape: [num_images, img_height * img_width * num_channels]
+
+    return layer_flat, num_features
+
+
 # Helper function to create FC layer
 def new_fc_layer(input, num_input, num_output, use_relu=True):
 
@@ -56,3 +73,60 @@ def new_fc_layer(input, num_input, num_output, use_relu=True):
         layer = tf.nn.relu(layer)
 
     return layer
+
+
+# Placeholder
+x = tf.placeholder(tf.float32, shape=[None, 128, 128, 3])
+x_image = x
+y = tf.placeholder(tf.float32, shape=[None, 1])
+
+# Conlutional Layer 1
+layer_conv1 = new_conv_layer(input=x_image,
+                             num_input_channels=3,
+                             filter_size=3,
+                             num_filters=32,
+                             use_pooling=True)
+
+# Conlutional Layer 1
+layer_conv2 = new_conv_layer(input=layer_conv1,
+                             num_input_channels=32,
+                             filter_size=3,
+                             num_filters=64,
+                             use_pooling=True)
+
+# Conlutional Layer 1
+layer_conv3 = new_conv_layer(input=layer_conv2,
+                             num_input_channels=64,
+                             filter_size=3,
+                             num_filters=128,
+                             use_pooling=True)
+
+# FC Layer 1
+layer_flat, num_features = flatten_layer(layer_conv3)
+
+layer_fc1 = new_fc_layer(input=layer_flat,
+                         num_inputs=num_features,
+                         num_outputs=100,
+                         use_relu=True)
+
+# FC layer 2
+layer_fc2 = new_fc_layer(input=layer_fc1,
+                         num_inputs=100,
+                         num_outputs=50,
+                         use_relu=True)
+
+# Out is steering angle
+y_pred = new_fc_layer(input=layer_fc2,
+                      num_inputs=50,
+                      num_outputs=1,
+                      use_relu=True)
+
+# Cost per image
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logist=y_pred, labels=y)
+
+# Total Cost of training batch
+cost = tf.reduce_mean(cross_entropy)
+
+
+# Optimization
+optimizer = tf.train.AdadeltaOptimizer(learning_rate=1e-4).minimize(cost)
