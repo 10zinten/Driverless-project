9853dbb3361915c153b7f5244c6a4d2fea30adaa
tenzin <ten13zin@gmail.com>
2017-09-25 22:30:52 +0530

    Create module for cnn_model1

--- steering-models/model1/cnn_model1.py
+++ steering-models/model1/cnn_model1.py
@@ -23,3 +23,43 @@ def max_pool2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                           strides=[1, 2, 2, 1], padding='SAME')
 
+
+class ConvModel1(object):
+    def __init__(self, is_training=True):
+        x = tf.placeholder(tf.float32, shape=[None, 128*128*3], name='x')
+        y_ = tf.placeholder(tf.float32, shape=[None, 1])
+        x_image = tf.reshape(x, [-1, 128, 128, 3])
+
+        # Conv layer 1
+        self.W_conv1 = weight_variable([3, 3, 1, 32])
+        self.b_conv1 = bias_variable([32])
+        self.h_conv1 = max_pool2x2(tf.nn.relu(conv2d(x_image, self.W_conv1, 1) + self.b_conv1))
+
+        # Conv layer 2
+        self.W_conv2 = weight_variable([3, 3, 32, 64])
+        self.b_conv2 = bias_variable([64])
+        self.h_conv2 = max_pool2x2(tf.nn.relu(conv2d(self.h_conv1, self.W_conv2, 1) + self.b_conv2))
+
+        # Conv layer 3
+        self.W_conv2 = weight_variable([3, 3, 64, 128])
+        self.b_conv2 = bias_variable([128])
+        self.h_conv2 = max_pool2x2(tf.nn.relu(conv2d(self.h_conv1, self.W_conv2, 1) + self.b_conv2))
+
+
+        # Fully Connect layer 1
+        self.W_fc1 = weight_variable([16*16*128, 1024])
+        self.b_fc1 = bias_variable([1024])
+
+        self.h_conv5_flat = tf.reshape(self.h_conv2, [-1, 16*16*128])
+        self.h_fc1 = tf.nn.relu(tf.matmul(self.h_conv5_flat, self.W_fc1) + self.b_fc1)
+        print("Fc1 output shape: ", self.h_fc1)
+
+        # Output layer
+        self.W_fc2 = weight_variable([1024, 1])
+        self.b_fc2 = bias_variable([1])
+        h_fc2 = tf.matmul(self.h_fc1, self.W_fc2) + self.b_fc2
+        print("Final output shape: ", h_fc2)
+
+        self.x = x
+        self.y_ = y_
+        self.y = h_fc2
