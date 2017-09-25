ef0e3200bc2169ec71af2f43f2a4f6dc4c370429
tenzin <ten13zin@gmail.com>
2017-09-25 22:15:23 +0530

    Create Cnn_model1 class module's weight and bias helper-function

---
+++ steering-models/model1/cnn_model1.py
@@ -0,0 +1,14 @@
+'''
+CNN model is based the architecture metioned in ../cnn_architecture.txt
+'''
+
+import tensorflow as tf
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape=shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
