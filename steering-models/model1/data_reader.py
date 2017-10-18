b95b122b97346dcb66eedc86e517a15e33299fe1
tenzin <ten13zin@gmail.com>
2017-10-18 12:25:29 +0530

    Seperate out features and label to feed in Tensor

--- steering-models/model1/data_reader.py
+++ steering-models/model1/data_reader.py
@@ -7,14 +7,25 @@ import numpy as np
 from .preprocess import TRAIN_DIR, CV_DIR, TEST_DIR
 
 
+def seperate_features_from_label(xs, ys, datasets):
+    for data_point in datasets:
+        xs.append(data_point[0])
+        ys.append(data_point[1])
+
+
 class DataReader(object):
     def __init(self):
         self.train_set = np.load(TRAIN_DIR)
+        self.xs = []
+        self.ys = []
+
+        # seperating feature xs from label ys
+        seperate_features_from_label(self.xs, self.ys, self.train_set)
 
     def train_batch(self, size=64):
         first_index, last_index = 0, size
         while last_index <= len(self.train_set) + size:
-            yield self.train_set[first_index: last_index, :, :, :]
+            yield np.array(self.xs[first_index: last_index]), np.array(self.ys[first_index: last_index])
             first_index, last_index = last_index, last_index + size
 
     def cv_set(self):
@@ -24,5 +35,7 @@ class DataReader(object):
 
     def test_set(self):
         test_set = np.load(TEST_DIR)
-        # loading angle from test.csv file
-        return test_set  # [test_set, csv]
+        xs = []
+        ys = []
+        seperate_features_from_label(xs, ys, test_set)
+        return np.array(xs), np.array(ys)
