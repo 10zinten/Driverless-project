a5384825bbd7f0d04d40283206462454412d89f8
tenzin <ten13zin@gmail.com>
2018-08-16 20:29:17 +0530

    add sloth annotation config

---
+++ myconfig.py
@@ -0,0 +1,22 @@
+LABELS = [
+    {
+        'attributes': {
+            'type': 'rect',
+            'class': 'orange'
+        },
+        'inserter': 'sloth.items.RectItemInserter',
+        'item':     'sloth.items.RectItem',
+        'hotkey':   'o',
+        'text':     'Orange',
+    },
+    {
+        'attributes': {
+            'type':  'rect',
+            'class': 'yellow'
+        },
+        'inserter': 'sloth.items.RectItemInserter',
+        'item':     'sloth.items.RectItem',
+        'hotkey':   'y',
+        'text':     'Yellow',
+    },
+]
