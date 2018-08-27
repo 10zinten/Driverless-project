f5cb57c24e53c2188a711b606f68100ab3ed1b35
tenzin <ten13zin@gmail.com>
2018-08-27 23:59:50 +0530

    add sturcture of the model

---
+++ object-detection/ssd-mobilenet/model/ssd-mobilenet.py
@@ -0,0 +1,24 @@
+import os
+
+import numpy as np
+import tensorflow as tf
+
+class SSDMobileNet:
+
+    def __init__(self, sess, preset):
+        self.preset = preset
+        self.sess = sess
+        self.__build = False
+        self.__build_names()
+
+    def build_from_mobilenet(self, num_classes, a_trous=True):
+        """ Build the model from MobileNet. """
+        pass
+
+    def __load_mobilenet(self, dir):
+        sess = self.sess
+        pass
+
+    def __build_names(self):
+        '''Name of the feature maps.'''
+        pass
