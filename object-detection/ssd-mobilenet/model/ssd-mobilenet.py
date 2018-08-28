d9971aaa2a09408c2b59341356623cd56e4ebeb6
tenzin <ten13zin@gmail.com>
2018-08-29 00:00:12 +0530

    base mobilenet build successful

--- object-detection/ssd-mobilenet/model/ssd-mobilenet.py
+++ object-detection/ssd-mobilenet/model/ssd-mobilenet.py
@@ -3,17 +3,31 @@ import os
 import numpy as np
 import tensorflow as tf
 
+from base_network import MobileNetBase
+from utils import parse_args
+
 class SSDMobileNet:
 
-    def __init__(self, sess, preset):
-        self.preset = preset
-        self.sess = sess
-        self.__build = False
-        self.__build_names()
+    def __init__(self, args):
+        self.sess = None
+        self.args = args
+        self.image_input = None
+        self.result = None
+        self.loss = None
+        self.localization_loss = None
+        self.confidence_loss = None
+        self.labels = None
+        self.losses = None
+        self.__build()
 
-    def build_from_mobilenet(self, num_classes, a_trous=True):
+    def __build(self):
+        self.build_from_mobilenet(2)
+
+    def build_from_mobilenet(self, num_classes):
         """ Build the model from MobileNet. """
-        pass
+        self.num_classes = num_classes + 1
+        base = MobileNetBase(self.args)
+
 
     def __load_mobilenet(self, dir):
         sess = self.sess
@@ -29,3 +43,7 @@ class SSDMobileNet:
     def __build_names(self):
         '''Name of the feature maps.'''
         pass
+
+if __name__ == "__main__":
+    config_args = parse_args()
+    ssd = SSDMobileNet(config_args)
