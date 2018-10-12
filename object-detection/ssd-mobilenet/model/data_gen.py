6a6084dfe970f1da9da6bfef3ee063461db47761
tenzin <ten13zin@gmail.com>
2018-10-13 04:20:25 +0530

    add: label creator transform & test: no passing

--- object-detection/ssd-mobilenet/model/data_gen.py
+++ object-detection/ssd-mobilenet/model/data_gen.py
@@ -8,6 +8,11 @@ import cv2
 import numpy as np
 
 from model.utils import get_filenames_and_labels
+from model.ssdutils import get_anchors_for_preset, get_preset_by_name, anchors2array
+from model.ssdutils import box2array, compute_overlap, compute_location
+
+
+preset = get_preset_by_name('ssdmobilenet160')
 
 class Transform:
     def __init__(self, **kwargs):
@@ -165,7 +170,69 @@ class TransformPickerTransform(Transform):
     def __repr__(self):
         return repr(self.transforms[self.pick])
 
-def build_transforms(data):
+
+###############################################################################
+#                     Label Creator Transform                                 #
+###############################################################################
+
+def process_overlap(idx, score, gt, anchor, vec, matches, num_classes):
+    """
+    i: datapoint id
+    idx: index of overlapped anchor
+    score: socre of overlapped acnhor
+    """
+    box, label = gt[:-1], gt[-1]
+    if idx in matches and matches[idx] >= score:
+        return
+
+    matches[idx] = score
+    vec[idx, 0:num_classes+1] = 0
+    vec[idx, label] = 1
+    vec[idx, num_classes+1:] = compute_location(box, anchor)
+
+class LabelCreatorTransform(Transform):
+    """
+    Create a label vector out of a ground truth of a sample.
+    Parameters: preset, num_classes
+    """
+    def initialize(self):
+        self.anchors = get_anchors_for_preset(self.preset)
+        self.img_size = self.preset.image_size
+        self.anchors_arr = anchors2array(self.anchors, self.img_size)
+        self.initialized = True
+
+    def __call__(self, data, label, gt):
+        if not self.initialized:
+            self.initialize()
+
+        vec = np.zeros((len(self.anchors), self.num_classes+5), dtype=np.float32)
+        vec[:, self.num_classes] = 1    # bg
+        vec[:, self.num_classes+1] = 0  # x offset
+        vec[:, self.num_classes+2] = 0  # y offset
+        vec[:, self.num_classes+3] = 0  # log width scale
+        vec[:, self.num_classes+4] = 0  # log height scale
+
+        overlaps = []
+        for box in gt:
+            box_arr = box2array(box[:-1], self.img_size)
+            overlaps.append(compute_overlap(box_arr, self.anchors_arr, 0.1))
+        print(overlaps)
+
+        matches = {}
+        for j, gt in enumerate(gt):
+            idxs, scores = overlaps[j]
+            for idx, score in zip(idxs, scores):
+                anchor = self.anchors[idx]
+                process_overlap(idx, score, gt, anchor, vec, matches, self.num_classes)
+
+        return data, vec, gt
+
+    def __repr__(self):
+        return "Label Creator Transform"
+
+
+
+def build_train_transforms(preset, num_classes):
 
     ####  Photomatic Distortions  ###
     brightness = BrightnessTransform(delta=100)
@@ -199,43 +266,47 @@ def build_transforms(data):
     horizontal_flip = HorizontalFlip()
     random_horizontal_flip = RandomTransform(prob=0.5, transform=horizontal_flip)
 
+    transforms = [
+            ImageLoaderTransform(),
+            random_brightness,
+            distort,
+            random_channels_reorder,
+            random_horizontal_flip,
+            LabelCreatorTransform(preset=preset, num_classes=num_classes)
+        ]
 
-    if data == 'train':
-        transforms = [
-                ImageLoaderTransform(),
-                random_brightness,
-                distort,
-                random_channels_reorder,
-                random_horizontal_flip,
-            ]
-    else:
-        transforms = [
-                ImageLoaderTransform()
-            ]
+    return transforms
+
+def build_val_transforms(preset, num_classes):
+
+    transforms = [
+            ImageLoaderTransform(),
+            LabelCreatorTransform(preset=preset, num_classes=num_classes)
+        ]
 
     return transforms
 
 class TrainingData:
 
     def __init__(self, image_dir, label_dir, param):
-        train_filenames, train_labels = get_filenames_and_labels(image_dir, label_dir, 'train')
+        train_filenames, train_label = get_filenames_and_labels(image_dir, label_dir, 'train')
         nones = [None] * len(train_filenames)
-        train_samples = list(zip(train_filenames, nones, train_labels))
-        val_filenames, val_labels = get_filenames_and_labels(image_dir, label_dir, 'dev')
+        train_samples = list(zip(train_filenames, nones, train_label))
+        val_filenames, val_label = get_filenames_and_labels(image_dir, label_dir, 'dev')
         nones = [None] * len(val_filenames)
-        val_samples = list(zip(val_filenames, nones, val_labels))
+        val_samples = list(zip(val_filenames, nones, val_label))
 
 
-        self.preset = None
-        self.num_class = None
-        self.train_tfs = build_transforms('train')
-        self.val_tfs = build_transforms('val')
+        self.preset = preset = get_preset_by_name('ssdmobilenet160')
+        self.num_classes = 2
+        self.train_tfs = build_train_transforms(self.preset, self.num_classes)
+        self.val_tfs = build_val_transforms(self.preset, self.num_classes)
         self.train_generator  = self.__build_generator(train_samples, self.train_tfs)
         self.val_generator    = self.__build_generator(val_samples, self.val_tfs)
-        self.num_train = None
-        self.num_val = None
-        self.train_samples = None
-        self.val_samples = None
+        self.num_train = len(train_samples)
+        self.num_val = len(val_samples)
+        self.train_samples = list(zip(train_filenames, train_label))
+        self.val_samples = list(zip(val_filenames, val_label))
 
     def __build_generator(self, all_samples_, transforms):
 
@@ -269,6 +340,7 @@ class TrainingData:
 
                 for transform in transforms:
                     print("[INFO] {} applied ... ok".format(transform))
+                print()
 
                 yield images, labels, gt_boxes
 
